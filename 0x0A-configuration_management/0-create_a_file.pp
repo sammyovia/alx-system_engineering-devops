e99d8ce030edefb59799f2945883aa582c83143c
sammyovia <sammyigbinovia@gmail.com>
2023-01-12 00:18:03 +0000

    hotfix

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # This Puppet manifest creates a file in /tmp
 
-file { 'holberton':
+file { 'school':
   ensure  => file,
-  path    => '/tmp/holberton',
+  path    => '/tmp/school',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
